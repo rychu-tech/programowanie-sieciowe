56edd8f1d9cd253f77427566a2d4b1a10908ec4e
root <rychudotka14@gmail.com>
2023-05-08 18:33:38 +0200

    Fixed some bugs

--- client.c
+++ client.c
@@ -1,152 +1,3 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <sys/socket.h>
-// #include <netinet/in.h>
-// #include <arpa/inet.h>
-// #include <unistd.h>
-// #include <net/if.h>
-// #include <netdb.h>
-
-// #define DHCP_SERVER_PORT 12345
-// #define DHCP_CLIENT_PORT 1234
-
-// typedef struct {
-//     uint8_t op;
-//     uint8_t htype;
-//     uint8_t hlen;
-//     uint8_t hops;
-//     uint32_t xid;
-//     uint16_t secs;
-//     uint16_t flags;
-//     struct in_addr ciaddr;
-//     struct in_addr yiaddr;
-//     struct in_addr siaddr;
-//     struct in_addr giaddr;
-//     uint8_t chaddr[16];
-//     uint8_t options[312];
-// } dhcp_message;
-
-// int main(int argc, char *argv[]) {
-//     if (argc != 2) {
-//         printf("Usage: %s <interface>\n", argv[0]);
-//         return 1;
-//     }
-
-//     int sockfd;
-//     struct sockaddr_in servaddr_send, servaddr_recv;
-//     socklen_t servaddr_recv_len = sizeof(servaddr_recv);
-//     struct ifreq ifr;
-//     struct in_addr subnet_mask, router_ip, server_ip;
-//     uint32_t lease_time, renewal_time, rebinding_time;
-//     dhcp_message dhcp_discover, dhcp_offer, dhcp_request, dhcp_ack;
-//     uint8_t *option_ptr;
-//     ssize_t len;
-
-//     // Create a socket for sending and receiving DHCP messages
-//     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-//     if (sockfd < 0) {
-//         perror("socket");
-//         return 1;
-//     }
-
-//     // Bind the socket to the client port
-//     struct sockaddr_in client_addr = { 0 };
-//     client_addr.sin_family = AF_INET;
-//     client_addr.sin_port = htons(DHCP_CLIENT_PORT);
-//     client_addr.sin_addr.s_addr = INADDR_ANY;
-//     if (bind(sockfd, (struct sockaddr*) &client_addr, sizeof(client_addr)) < 0) {
-//         perror("bind");
-//         return 1;
-//     }
-
-//     // Set the interface for sending and receiving DHCP messages
-//     strncpy(ifr.ifr_name, argv[1], IFNAMSIZ);
-
-//     int broadcastEnable = 1;
-//     setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
-
-//     // if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0) {
-//     //     perror("setsockopt");
-//     //     return 1;
-//     // }
-
-//     // Set up the server address for sending DHCP messages
-//     memset(&servaddr_send, 0, sizeof(servaddr_send));
-//     servaddr_send.sin_family = AF_INET;
-//     servaddr_send.sin_port = htons(DHCP_SERVER_PORT);
-//     inet_pton(AF_INET, "255.255.255.255", &servaddr_send.sin_addr);
-
-//     // Set up the DHCP discover message
-//     memset(&dhcp_discover, 0, sizeof(dhcp_message));
-//     dhcp_discover.op = 1; // BOOTREQUEST
-//     dhcp_discover.htype = 1; // Ethernet
-//     dhcp_discover.hlen = 6; // MAC address length
-//     dhcp_discover.xid = random();
-//     dhcp_discover.flags = htons(0x8000); // Broadcast flag
-//     memcpy(dhcp_discover.chaddr, &ifr.ifr_hwaddr.sa_data, 6);
-
-//     option_ptr = dhcp_discover.options;
-//     *option_ptr++ = 53; // DHCP message type option
-//     *option_ptr++ = 1; // Length
-//     *option_ptr++ = 1; // DHCP
-// // Send the DHCP discover message
-// printf("Sending DHCP discover message...\n");
-// if (sendto(sockfd, &dhcp_discover, sizeof(dhcp_message), 0, (struct sockaddr*) &servaddr_send, sizeof(servaddr_send)) < 0) {
-//     perror("sendto");
-//     return 1;
-// }
-
-// // Wait for a DHCP offer message from the server
-// printf("Waiting for DHCP offer message...\n");
-// len = recvfrom(sockfd, &dhcp_offer, sizeof(dhcp_message), 0, (struct sockaddr*) &servaddr_recv, &servaddr_recv_len);
-// if (len < 0) {
-//     perror("recvfrom");
-//     return 1;
-// }
-
-// // Parse the DHCP offer message
-// printf("Received DHCP offer message.\n");
-// if (dhcp_offer.op != 2 || dhcp_offer.htype != 1 || dhcp_offer.hlen != 6 || dhcp_offer.xid != dhcp_discover.xid) {
-//     printf("Invalid DHCP offer message.\n");
-//     return 1;
-// }
-
-// option_ptr = dhcp_offer.options;
-// while (*option_ptr != 0xFF) {
-//     if (*option_ptr == 1) { // Subnet mask
-//         memcpy(&subnet_mask, option_ptr + 2, sizeof(subnet_mask));
-//     } else if (*option_ptr == 3) { // Router IP
-//         memcpy(&router_ip, option_ptr + 2, sizeof(router_ip));
-//     } else if (*option_ptr == 51) { // Lease time
-//         memcpy(&lease_time, option_ptr + 2, sizeof(lease_time));
-//         lease_time = ntohl(lease_time);
-//     } else if (*option_ptr == 58) { // Renewal time
-//         memcpy(&renewal_time, option_ptr + 2, sizeof(renewal_time));
-//         renewal_time = ntohl(renewal_time);
-//     } else if (*option_ptr == 59) { // Rebinding time
-//         memcpy(&rebinding_time, option_ptr + 2, sizeof(rebinding_time));
-//         rebinding_time = ntohl(rebinding_time);
-//     }
-
-//     option_ptr += option_ptr[1] + 2;
-// }
-
-// // Print the DHCP lease information
-// printf("DHCP lease information:\n");
-// printf("IP address: %s\n", inet_ntoa(dhcp_ack.yiaddr));
-// printf("Subnet mask: %s\n", inet_ntoa(subnet_mask));
-// printf("Router IP: %s\n", inet_ntoa(router_ip));
-// printf("Lease time: %u seconds\n", lease_time);
-// printf("Renewal time: %u seconds\n", renewal_time);
-// printf("Rebinding time: %u seconds\n", rebinding_time);
-
-// // Close the socket
-// close(sockfd);
-
-// return 0;
-// }
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -155,129 +6,145 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <time.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
 
-#define SERVER_PORT 12345
-#define CLIENT_PORT 1234
-#define MAXLINE 1024
+#define DHCP_SERVER_PORT 67
+#define DHCP_CLIENT_PORT 68
+#define DHCP_MAGIC_COOKIE 0x63825363
+#define DHCP_DISCOVER_MSG_TYPE 0x01
+#define SERVER_ADDR "255.255.255.255"
 
-struct dhcp_msg {
-    uint8_t op, htype, hlen, hops;
+typedef struct {
+    uint8_t opcode;
+    uint8_t htype;
+    uint8_t hlen;
+    uint8_t hops;
     uint32_t xid;
-    uint16_t secs, flags;
-    uint32_t ciaddr, yiaddr, siaddr, giaddr;
+    uint16_t secs;
+    uint16_t flags;
+    struct in_addr ciaddr;
+    struct in_addr yiaddr;
+    struct in_addr siaddr;
+    struct in_addr giaddr;
     uint8_t chaddr[16];
     uint8_t sname[64];
     uint8_t file[128];
-    uint32_t magic_cookie;
-    uint8_t options[312];
-};
+    uint32_t cookie;
+} dhcp_packet;
 
-int main(int argc, char *argv[]) {
+void print_offer_packet(dhcp_packet* packet) {
+    printf("=== DHCP Offer ===\n");
+    printf("Opcode: 0x%02X\n", packet->opcode);
+    printf("Hardware Type: 0x%02X\n", packet->htype);
+    printf("Hardware Length: 0x%02X\n", packet->hlen);
+    printf("Hops: 0x%02X\n", packet->hops);
+    printf("Transaction ID: 0x%08X\n", packet->xid);
+    printf("Seconds Elapsed: 0x%04X\n", packet->secs);
+    printf("Flags: 0x%04X\n", packet->flags);
+    printf("Client IP Address: %s\n", inet_ntoa(packet->ciaddr));
+    printf("Your IP Address: %s\n", inet_ntoa(packet->yiaddr));
+    printf("Server IP Address: %s\n", inet_ntoa(packet->siaddr));
+    printf("Gateway IP Address: %s\n", inet_ntoa(packet->giaddr));
+    printf("Client Hardware Address: ");
+    for (int i = 0; i < packet->hlen; i++) {
+        printf("%02X:", packet->chaddr[i]);
+    }
+    printf("\b \n"); // backspace to remove last ':'
+    printf("Server Host Name: %s\n", packet->sname);
+    printf("Boot File Name: %s\n", packet->file);
+    printf("Magic Cookie: 0x%08X\n", packet->cookie);
+    return;
+}
+
+int main(int argc, char **argv) {
     int sockfd;
-    char buffer[MAXLINE];
-    struct sockaddr_in servaddr_send, servaddr_recv;
-
-    // Creating socket file descriptor
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (sockfd < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
+        perror("socket");
+        exit(1);
     }
 
-    // Setting SO_BROADCAST option on socket
+    struct ifreq ifr;
+    strcpy(ifr.ifr_name, argv[1]);
+    if (ioctl(sockfd, SIOCGIFHWADDR, &ifr) < 0) {
+        perror("ioctl failed");
+        exit(EXIT_FAILURE);
+    }
+    unsigned char *macaddr = (unsigned char *)ifr.ifr_hwaddr.sa_data;
+
+
+    struct sockaddr_in serveraddr;
+    dhcp_packet discover_packet;
+    dhcp_packet offer_packet;
+    memset(&discover_packet, 0, sizeof(discover_packet));
+
+    discover_packet.opcode = 0x01; // BOOTREQUEST
+    discover_packet.htype = 0x01; // Ethernet
+    discover_packet.hlen = 0x06; // Hardware address length
+    discover_packet.hops = 0x00; // Hops
+    discover_packet.xid = rand(); // Transaction ID
+    discover_packet.secs = 0x0000; // Seconds elapsed
+    discover_packet.flags = htons(0x8000); // Broadcast flag
+    discover_packet.ciaddr.s_addr = 0; // Client IP address (0.0.0.0)
+    discover_packet.yiaddr.s_addr = 0; // Your IP address (0.0.0.0)
+    discover_packet.siaddr.s_addr = 0; // Server IP address (0.0.0.0)
+    discover_packet.giaddr.s_addr = 0; // Gateway IP address (0.0.0.0)
+    memset(discover_packet.chaddr, 0, sizeof(discover_packet.chaddr)); // Client hardware address
+    memcpy(discover_packet.chaddr, macaddr, 6); // Replace with your own MAC address
+    memset(discover_packet.sname, 0, sizeof(discover_packet.sname)); // Server host name
+    memset(discover_packet.file, 0, sizeof(discover_packet.file)); // Boot file name
+    discover_packet.cookie = htonl(DHCP_MAGIC_COOKIE); // Magic cookie
+
+    // Option 53: DHCP Message Type
+    uint8_t dhcp_msg_type[3] = {0x35, 0x01, DHCP_DISCOVER_MSG_TYPE};
+    // Option 55: Parameter Request List
+    uint8_t dhcp_param_req_list[9] = {0x37, 0x09, 0x01, 0x03, 0x06, 0x0f, 0x1f, 0x21, 0xff};
+    // Option 255: End
+    uint8_t dhcp_end[1] = {0xff};
+
+    char buffer[1024];
+
+    
+
+    // Enable broadcast
     int broadcastEnable = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
-
-    // Filling server information
-    memset(&servaddr_send, 0, sizeof(servaddr_send));
-    memset(&servaddr_recv, 0, sizeof(servaddr_recv));
-
-    servaddr_send.sin_family = AF_INET;
-    servaddr_send.sin_port = htons(SERVER_PORT);
-    inet_pton(AF_INET, "0.0.0.0", &servaddr_send.sin_addr);
-
-    servaddr_recv.sin_family = AF_INET;
-    servaddr_recv.sin_port = htons(CLIENT_PORT);
-    inet_pton(AF_INET, "0.0.0.0", &servaddr_recv.sin_addr);
-
-    // Binding the socket with the client address
-    if (bind(sockfd, (const struct sockaddr *)&servaddr_recv, sizeof(servaddr_recv)) < 0) {
-        perror("bind failed");
+    if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable)) < 0) {
+        perror("setsockopt failed");
         exit(EXIT_FAILURE);
     }
 
-    struct dhcp_msg dhcp_message;
-    memset(&dhcp_message, 0, sizeof(dhcp_message));
+    serveraddr.sin_family = AF_INET;
+       serveraddr.sin_port = htons(DHCP_SERVER_PORT);
+    serveraddr.sin_addr.s_addr = inet_addr(SERVER_ADDR); // Broadcast address
 
-    dhcp_message.op = 1;
-    dhcp_message.htype = 1;
-    dhcp_message.hlen = 6;
-    dhcp_message.hops = 0;
+    // Construct the DHCP message
+    memcpy(buffer, &discover_packet, sizeof(dhcp_packet));
+    memcpy(buffer + sizeof(dhcp_packet), dhcp_msg_type, sizeof(dhcp_msg_type));
+    memcpy(buffer + sizeof(dhcp_packet) + sizeof(dhcp_msg_type), dhcp_param_req_list, sizeof(dhcp_param_req_list));
+    memcpy(buffer + sizeof(dhcp_packet) + sizeof(dhcp_msg_type) + sizeof(dhcp_param_req_list), dhcp_end, sizeof(dhcp_end));
+    int packet_size = sizeof(dhcp_packet) + sizeof(dhcp_msg_type) + sizeof(dhcp_param_req_list) + sizeof(dhcp_end);
 
-    dhcp_message.xid = htonl(rand());
-    dhcp_message.secs = htons(0);
-    dhcp_message.flags = htons(0x8000);
+    // Send the DHCP discover message
+    if (sendto(sockfd, buffer, packet_size, 0, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) {
+        perror("sendto");
+        exit(1);
+    }
 
-    dhcp_message.chaddr[0] = 0x00;
-    dhcp_message.chaddr[1] = 0x0c;
-    dhcp_message.chaddr[2] = 0x29;
-    dhcp_message.chaddr[3] = 0x9d;
-    dhcp_message.chaddr[4] = 0x1d;
-    dhcp_message.chaddr[5] = 0xa9;
+    printf("DHCP Discover sent to %s\n", inet_ntoa(serveraddr.sin_addr));
 
-    dhcp_message.magic_cookie = htonl(0x63825363);
-
-    dhcp_message.options[0] = 53; // Option 53 (DHCP Message Type)
-    dhcp_message.options[1] = 1; // Option length
-    dhcp_message.options[2] = 1; // DHCPDISCOVER
-
-    dhcp_message.options[3] = 255; // End of options
-    // Sending the DHCP Discover message
     int n;
     socklen_t len;
-    sendto(sockfd, &dhcp_message, sizeof(dhcp_message), 0, (const struct sockaddr *)&servaddr_send, sizeof(servaddr_send));
-
-    // Receiving the DHCP Offer message
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&servaddr_recv, &len);
-    buffer[n] = '\0';
-
-    // Parsing the DHCP Offer message
-    struct dhcp_msg dhcp_offer;
-    memset(&dhcp_offer, 0, sizeof(dhcp_offer));
-    memcpy(&dhcp_offer, buffer, sizeof(dhcp_offer));
-
-    uint32_t offered_ip = dhcp_offer.yiaddr;
-    uint32_t subnet_mask = 0;
-    uint32_t router_ip = 0;
-    uint32_t lease_time = 0;
-    uint32_t renewal_time = 0;
-    uint32_t rebinding_time = 0;
-
-    // Extracting subnet mask, router IP, lease time, renewal time, and rebinding time from DHCP Offer options
-    uint8_t *options_ptr = dhcp_offer.options;
-    while (*options_ptr != 255) {
-        if (*options_ptr == 1) { // Subnet Mask option
-            subnet_mask = *(uint32_t *)(options_ptr + 2);
-        } else if (*options_ptr == 3) { // Router option
-            router_ip = *(uint32_t *)(options_ptr + 2);
-        } else if (*options_ptr == 51) { // Lease Time option
-            lease_time = ntohl(*(uint32_t *)(options_ptr + 2));
-        } else if (*options_ptr == 58) { // Renewal Time option
-            renewal_time = ntohl(*(uint32_t *)(options_ptr + 2));
-        } else if (*options_ptr == 59) { // Rebinding Time option
-            rebinding_time = ntohl(*(uint32_t *)(options_ptr + 2));
-        }
-        options_ptr += options_ptr[1] + 2;
+    n = recvfrom(sockfd, &offer_packet, sizeof(offer_packet), 0, (struct sockaddr *)&serveraddr, &len);
+    if (n < 0) {
+        perror("recvfrom failed");
+        exit(EXIT_FAILURE);
     }
 
-    // Printing the offered IP address, subnet mask, router IP, lease time, renewal time, and rebinding time
-    printf("Offered IP address: %s\n", inet_ntoa(*(struct in_addr *)&offered_ip));
-    printf("Subnet mask: %s\n", inet_ntoa(*(struct in_addr *)&subnet_mask));
-    printf("Router IP: %s\n", inet_ntoa(*(struct in_addr *)&router_ip));
-    printf("Lease time: %d seconds\n", lease_time);
-    printf("Renewal time: %d seconds\n", renewal_time);
-    printf("Rebinding time: %d seconds\n", rebinding_time);
+    printf("DHCP Offer received from %s\n", inet_ntoa(serveraddr.sin_addr));
+    print_offer_packet(&offer_packet);
+    close(sockfd);
 
-return 0;
+    return 0;
 }
+
--- server.c
+++ server.c
@@ -1,136 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
-#define DHCP_SERVER_PORT 12345
-#define DHCP_CLIENT_PORT 1234
+#define DHCP_SERVER_PORT 67
+#define DHCP_CLIENT_PORT 68
+#define DHCP_MAGIC_COOKIE 0x63825363
+#define DHCP_DISCOVER_MSG_TYPE 0x01
+#define DHCP_OFFER_MSG_TYPE 0x02
+#define DHCP_NETMASK_OPTION 0x01
+#define DHCP_DNS_OPTION 0x06
+#define DHCP_SERVER_ID_OPTION 0x36
+#define DHCP_LEASE_TIME_OPTION 0x33
+#define SERVER_ADDR "0.0.0.0"
 
 typedef struct {
-    uint8_t op;         // Message opcode
-    uint8_t htype;      // Hardware address type
-    uint8_t hlen;       // Hardware address length
-    uint8_t hops;       // Number of relay agent hops
-    uint32_t xid;       // Transaction ID
-    uint16_t secs;      // Seconds since boot
-    uint16_t flags;     // Flags
-    uint32_t ciaddr;    // Client IP address
-    uint32_t yiaddr;    // Your (client) IP address
-    uint32_t siaddr;    // Server IP address
-    uint32_t giaddr;    // Relay agent IP address
-    uint8_t chaddr[16]; // Client hardware address
-    uint8_t padding[192];
-    uint32_t magic_cookie; // Magic cookie
-    uint8_t options[64];   // Options
-} dhcp_message;
+    uint8_t opcode;
+    uint8_t htype;
+    uint8_t hlen;
+    uint8_t hops;
+    uint32_t xid;
+    uint16_t secs;
+    uint16_t flags;
+    struct in_addr ciaddr;
+    struct in_addr yiaddr;
+    struct in_addr siaddr;
+    struct in_addr giaddr;
+    uint8_t chaddr[16];
+    uint8_t sname[64];
+    uint8_t file[128];
+    uint32_t cookie;
+} dhcp_packet;
 
-int main(int argc, char* argv[]) {
-    int sockfd, broadcast = 1;
-    struct sockaddr_in servaddr, cliaddr;
-    dhcp_message dhcp_offer;
-    uint8_t mac_addr[6] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55}; // Change this to your own MAC address
-    uint32_t ip_addr = htonl(0xc0a80101); // Change this to your own IP address
-    uint32_t netmask = htonl(0xffffff00); // Change this to your network mask
-    uint32_t router_ip = htonl(0xc0a80101); // Change this to your router's IP address
-    uint32_t dns_ip = htonl(0xc0a80101); // Change this to your DNS server's IP address
-    uint32_t lease_time = htonl(86400); // Change this to the lease time in seconds
+typedef struct {
+    uint8_t option;
+    uint8_t length;
+    uint8_t value[256];
+} dhcp_option;
 
-    // Create a UDP socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+void send_dhcp_offer(int sockfd, struct sockaddr_in clientaddr, dhcp_packet discover_packet) {
+    dhcp_packet offer_packet;
+
+    memset(&offer_packet, 0, sizeof(offer_packet));
+
+    offer_packet.opcode = 0x02; // BOOTREPLY
+    offer_packet.htype = 0x01; // Ethernet
+    offer_packet.hlen = 0x06; // Hardware address length
+    offer_packet.hops = 0x00; // Hops
+    offer_packet.xid = discover_packet.xid; // Transaction ID
+    offer_packet.secs = 0x0000; // Seconds elapsed
+    offer_packet.flags = discover_packet.flags; // Unicast flag
+    offer_packet.ciaddr.s_addr = 0; // Client IP address (0.0.0.0)
+    offer_packet.yiaddr.s_addr = inet_addr("192.168.1.10"); // Offered IP address
+    offer_packet.siaddr.s_addr = inet_addr(SERVER_ADDR); // Server IP address
+    offer_packet.giaddr.s_addr = discover_packet.giaddr.s_addr; // Gateway IP address
+    memcpy(offer_packet.chaddr, discover_packet.chaddr, sizeof(offer_packet.chaddr)); // Client hardware address
+    // memset(offer_packet.sname, 0, sizeof(offer_packet.sname)); // Server host name
+    // memset(offer_packet.file, 0, sizeof(offer_packet.file)); // Boot file name
+    offer_packet.cookie = htonl(DHCP_MAGIC_COOKIE); // Magic cookie
+
+    // Option 53: DHCP Message Type
+    dhcp_option dhcp_msg_type = {0x35, 0x01, DHCP_OFFER_MSG_TYPE};
+    // Option 1: Subnet Mask
+    uint8_t dhcp_netmask[6] = {0x01, 0x04, 0xff, 0xff, 0xff, 0x00}; // 255.255.255.0
+    // Option 6: DNS Server
+    uint8_t dhcp_dns[6] = {0x06, 0x04, 0xc0, 0xa8, 0x01, 0x01}; // 192.168.1.1
+    // Option 51: IP Address Lease Time
+    uint8_t dhcp_lease_time[6] = {0x33, 0x04, 0x00, 0x01, 0x51, 0x80}; // 86400 seconds (1 day)
+    memcpy(&offer_packet.cookie + sizeof(offer_packet.cookie), &dhcp_msg_type, sizeof(dhcp_option));
+    memcpy(&offer_packet.cookie + sizeof(offer_packet.cookie) + sizeof(dhcp_option), dhcp_netmask, sizeof(dhcp_netmask));
+    memcpy(&offer_packet.cookie + sizeof(offer_packet.cookie) + sizeof(dhcp_option) + sizeof(dhcp_netmask), dhcp_dns, sizeof(dhcp_dns));
+
+    // Send DHCP offer packet
+    sendto(sockfd, &offer_packet, sizeof(offer_packet), 0, (struct sockaddr*)&clientaddr, sizeof(clientaddr));
+}
+int main(int argc, char *argv[]) {
+    int sockfd;
+    struct sockaddr_in serveraddr, clientaddr;
+    dhcp_packet discover_packet;
+    sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (sockfd < 0) {
         perror("socket");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
-    // Set the socket to allow broadcast
-    if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast)) < 0) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(DHCP_SERVER_PORT);
+    serveraddr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
 
-    // Bind the socket to the DHCP server port
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(DHCP_SERVER_PORT);
-
-    if (bind(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr)) < 0) {
+    if (bind(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) {
         perror("bind");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
-    uint32_t ip_pool[] = {
-        htonl(0xc0a80102), // 192.168.1.2
-        htonl(0xc0a80103), // 192.168.1.3
-        htonl(0xc0a80104), // 192.168.1.4
-        htonl(0xc0a80105)  // 192.168.1.5
-    };
-    int poolInx = 0;
-    int numPoolIps = sizeof(ip_pool) / sizeof(ip_pool[0]);
+    printf("Listening for DHCP requests...\n");
 
     while (1) {
-        // Receive a DHCP discover message from a client
-        memset(&dhcp_offer, 0, sizeof(dhcp_message));
-        socklen_t len = sizeof(cliaddr);
-        if (recvfrom(sockfd, &dhcp_offer, sizeof(dhcp_message), 0, (struct sockaddr*) &cliaddr, &len) < 0) {
+        memset(&discover_packet, 0, sizeof(discover_packet));
+
+        int n = recvfrom(sockfd, &discover_packet, sizeof(discover_packet), 0, (struct sockaddr *)&clientaddr, &(socklen_t){sizeof(clientaddr)});
+        if (n < 0) {
             perror("recvfrom");
-        continue;
+            exit(1);
+        }
+
+        if (discover_packet.opcode == 0x01 && discover_packet.htype == 0x01 && discover_packet.hlen == 0x06 && discover_packet.cookie == htonl(DHCP_MAGIC_COOKIE)) {
+            printf("Received DHCP Discover message from %s\n", inet_ntoa(clientaddr.sin_addr));
+
+            send_dhcp_offer(sockfd, clientaddr, discover_packet);
+
+            printf("Sent DHCP Offer message to %s\n", inet_ntoa(clientaddr.sin_addr));
+        }
     }
 
-    // Check that the message is a DHCP discover message
-    if (dhcp_offer.op != 1 || dhcp_offer.htype != 1 || dhcp_offer.hlen != 6 || dhcp_offer.magic_cookie != htonl(0x63825363)) {
-        continue;
-    }
-
-    if (poolInx >= numPoolIps) {
-        printf("No more available addresses.\n");
-        continue;
-        
-    }
-
-    // Fill in the DHCP offer message
-    memset(&dhcp_offer, 0, sizeof(dhcp_message));
-    dhcp_offer.op = 2; // Boot reply
-    dhcp_offer.htype = 1; // Ethernet
-    dhcp_offer.hlen = 6; // Ethernet MAC address length
-    dhcp_offer.xid = rand(); // Transaction ID
-    dhcp_offer.yiaddr = ip_pool[poolInx]; // Offered IP address
-    dhcp_offer.siaddr = htonl(ip_addr); // Server IP address
-    dhcp_offer.magic_cookie = htonl(0x63825363); // Magic cookie
-
-    // Add DHCP options
-    uint8_t* option_ptr = dhcp_offer.options;
-    *option_ptr++ = 53; // DHCP message type
-    *option_ptr++ = 1;
-    *option_ptr++ = 2; // DHCP offer
-    *option_ptr++ = 1; // Subnet mask
-    *option_ptr++ = 4;
-    memcpy(option_ptr, &netmask, sizeof(netmask));
-    option_ptr += 4;
-    *option_ptr++ = 3; // Router option
-    *option_ptr++ = 4;
-    memcpy(option_ptr, &router_ip, sizeof(router_ip));
-    option_ptr += 4;
-    *option_ptr++ = 6; // DNS server option
-    *option_ptr++ = 4;
-    memcpy(option_ptr, &dns_ip, sizeof(dns_ip));
-    option_ptr += 4;
-    *option_ptr++ = 51; // IP address lease time
-    *option_ptr++ = 4;
-    memcpy(option_ptr, &lease_time, sizeof(lease_time));
-    option_ptr += 4;
-    *option_ptr++ = 255; // End option
-
-    // Send the DHCP offer message to the client
-    if (sendto(sockfd, &dhcp_offer, sizeof(dhcp_message), 0, (struct sockaddr*) &cliaddr, sizeof(cliaddr)) < 0) {
-        perror("sendto");
-        continue;
-    }
-    poolInx++;
-}
-
-return 0;
+    return 0;
 }
