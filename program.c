f9eb5a80238d73e10fc28231df4609ef2f87625e
rychu-tech <61971646+rychu-tech@users.noreply.github.com>
2023-05-14 15:41:58 +0200

    Add files via upload

--- program.c
+++ program.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <time.h>
 
 #define SERVER_PORT 67
 #define DHCP_CLIENT_PORT 68
@@ -11,9 +12,24 @@
 #define DHCP_OFFER 2
 #define DHCP_REQUEST 3
 #define DHCP_ACK 5
-#define DHCP_IP_RANGE_START "192.168.1.3"
+#define DHCP_IP_RANGE_START "192.168.1.2"
 #define DHCP_SERVER_IP "192.168.1.1"
+#define LEASE_TIME 86400 // 24 hours
+#define RENEWAL_TIME 43200 // 12 hours
 
+#define IP_POOL_SIZE 254 // Without server address and 0 address
+
+typedef enum {
+    FREE,
+    TAKEN,
+    IN_PROGRESS
+} ip_status;
+
+typedef struct {
+    uint8_t octets[4];
+    ip_status status;
+    time_t *timestamp;
+} ip_address;
 
 typedef struct {
     uint8_t op;
@@ -34,12 +50,65 @@ typedef struct {
     uint8_t options[308];
 } dhcp_packet;
 
+void initialize_ip_pool(ip_address* pool) {
+    for (int i = 2; i < IP_POOL_SIZE; i++) {
+        pool[i-2].octets[0] = 192;
+        pool[i-2].octets[1] = 168;
+        pool[i-2].octets[2] = 1;
+        pool[i-2].octets[3] = i;
+        pool[i-2].status = FREE;
+        pool[i-2].timestamp = NULL;
+    }
+}
+
+void print_ip_address(ip_address addr) {
+    printf("%d.%d.%d.%d\n", addr.octets[0],addr.octets[1],addr.octets[2],addr.octets[3]);
+}
+
+void print_ip_pool(ip_address* pool) {
+    for (int i = 0; i < IP_POOL_SIZE - 2; i++) {
+        printf("IP Address: ");
+        print_ip_address(pool[i]);
+        printf(" Status: %d", pool[i].status);
+        if (pool[i].timestamp != NULL) {
+            printf(" Timestamp: %ld", *pool[i].timestamp);
+        }
+        printf("\n");
+    }
+}
+
+void clean_ip_pool(ip_address* pool) {
+    for (int i = 0; i < IP_POOL_SIZE - 2; i++) {
+        if (pool[i].timestamp != NULL) {
+            if (*pool[i].timestamp + RENEWAL_TIME <= time(NULL)) {
+                pool[i].timestamp = NULL;
+                pool[i].status = FREE;
+                printf("IP POOL CLEANED UP");
+            }
+        }
+    }
+}
+
 
 void print_mac_address(const unsigned char* mac_address) {
     printf("MAC address: %02x:%02x:%02x:%02x:%02x:%02x\n", mac_address[0], mac_address[1], mac_address[2], mac_address[3], mac_address[4], mac_address[5]);
 }
 
-int create_offer_packet(dhcp_packet *offer_packet, dhcp_packet *discovery_packet) {
+ip_address* get_free_ip(ip_address* pool) {
+    for (int i = 0; i < IP_POOL_SIZE - 2; i++) {
+        if (pool[i].status == FREE) {
+            if (pool[i].timestamp == NULL) {
+                pool[i].timestamp = malloc(sizeof(time_t));
+            }
+            pool[i].status = IN_PROGRESS;
+            *(pool[i].timestamp) = time(NULL);
+            return &pool[i];
+        }
+    }
+    return NULL;
+}
+
+int create_offer_packet(dhcp_packet *offer_packet, dhcp_packet *discovery_packet, ip_address* pool) {
     memset(offer_packet, 0, sizeof(dhcp_packet));
     offer_packet->op = 2; // Boot reply
     offer_packet->htype = 1; // Ethernet
@@ -49,7 +118,17 @@ int create_offer_packet(dhcp_packet *offer_packet, dhcp_packet *discovery_packet
     printf("%d\n", discovery_packet->xid);
     offer_packet->secs = 0; // No seconds elapsed
     offer_packet->flags = htons(0x8000); // Broadcast flag
-    offer_packet->yiaddr.s_addr = inet_addr(DHCP_IP_RANGE_START); // Offered IP address
+
+    ip_address* free_ip = get_free_ip(pool);
+    char* ip_str = malloc(16 * sizeof(char));
+    if (free_ip != NULL) {
+        sprintf(ip_str, "%d.%d.%d.%d", free_ip->octets[0], free_ip->octets[1], free_ip->octets[2], free_ip->octets[3]);
+        offer_packet->yiaddr.s_addr = inet_addr(ip_str);
+        
+    } else {
+        printf("No free addresses available!\n");
+    }
+    // offer_packet->yiaddr.s_addr = inet_addr(DHCP_IP_RANGE_START); // Offered IP address
     offer_packet->siaddr.s_addr = inet_addr(DHCP_SERVER_IP); // Server address 
     offer_packet->ciaddr.s_addr = 0;
     offer_packet->giaddr.s_addr = 0;
@@ -60,7 +139,6 @@ int create_offer_packet(dhcp_packet *offer_packet, dhcp_packet *discovery_packet
     offer_packet->cookie = htonl(0x63825363);
 
     print_mac_address(offer_packet->chaddr);
-    int offset = 0;
     offer_packet->options[0] = 53; // DHCP message type option
     offer_packet->options[1] = 1; // Length of the option data
     offer_packet->options[2] = 2; // DHCP offer message type
@@ -79,32 +157,30 @@ int create_offer_packet(dhcp_packet *offer_packet, dhcp_packet *discovery_packet
     offer_packet->options[13] = 1; //
     offer_packet->options[14] = 1; //
 
-    // offer_packet->options[15] = 51; // Ip address lease time option
-    // offer_packet->options[16] = 4; // Option data length
-    // offer_packet->options[17] = 0;
-    // offer_packet->options[18] = 0;
-    // offer_packet->options[19] = 1;
-    // offer_packet->options[20] = 1; // Lease time in seconds
-
-    // offer_packet->options[21] = 6; // Ip address lease time option
-    // offer_packet->options[22] = 4; // Option data length
-    // offer_packet->options[23] = 192;
-    // offer_packet->options[24] = 160;
-    // offer_packet->options[25] = 1;
-    // offer_packet->options[26] = 1; // Lease time in seconds
-
-    offer_packet->options[15] = 54; // Dhcp server identity option
+    offer_packet->options[15] = 51; // Lease Time option
     offer_packet->options[16] = 4; // Option data length
-    offer_packet->options[17] = 192;
-    offer_packet->options[18] = 168;
-    offer_packet->options[19] = 1;
-    offer_packet->options[20] = 1; // Lease time in seconds
-
-    offer_packet->options[21] = 255; // End of options marker
+    *(uint32_t *)(&offer_packet->options[17]) = htonl(LEASE_TIME);
+    offer_packet->options[21] = 58; // Renewal Time option
+    offer_packet->options[22] = 4; // Option data length
+    *(uint32_t *)(&offer_packet->options[23]) = htonl(RENEWAL_TIME); // Convert renewal time to network byte order and store in packet
+    offer_packet->options[27] = 255; // End option
     return sizeof(dhcp_packet);
 }
 
-int create_ack_packet(dhcp_packet *ack_packet, dhcp_packet *request_packet) {
+
+uint32_t extract_requested_ip(dhcp_packet *packet) {
+    uint8_t *p = packet->options;
+    while (*p != 0xff) {
+        if (*p == 50) {
+            return *(uint32_t *)(p + 2);
+        }
+        p += p[1] + 2;
+    }
+    return 0;
+}
+
+int create_ack_packet(dhcp_packet *ack_packet, dhcp_packet *request_packet, uint32_t requested_ip, ip_address* pool) {
+    printf("Requested IP address: %s\n", inet_ntoa(*(struct in_addr *)&requested_ip));
     memset(ack_packet, 0, sizeof(dhcp_packet));
     ack_packet->op = 2; // Boot reply
     ack_packet->htype = 1; // Ethernet
@@ -113,7 +189,34 @@ int create_ack_packet(dhcp_packet *ack_packet, dhcp_packet *request_packet) {
     ack_packet->xid = request_packet->xid;
     ack_packet->secs = 0; // No seconds elapsed
     ack_packet->flags = htons(0x8000); // Broadcast flag
-    ack_packet->yiaddr.s_addr = inet_addr(DHCP_IP_RANGE_START); // Assigned IP address
+
+    int isFree = 1;
+    char* ip_str = malloc(16 * sizeof(char));
+    for (int i = 0; i < IP_POOL_SIZE - 2; i++) {
+        sprintf(ip_str, "%d.%d.%d.%d", pool[i].octets[0], pool[i].octets[1], pool[i].octets[2], pool[i].octets[3]);
+        // printf("%s\n\n", ip_str);
+        // printf("%s\n\n", inet_ntoa(*(struct in_addr *)&requested_ip));
+        if (strcmp(inet_ntoa(*(struct in_addr *)&requested_ip),ip_str) == 0) {
+            isFree = 0;
+            break;
+        }
+    }
+    if (isFree == 0) {
+        ip_address* free_ip = get_free_ip(pool);
+        char* ip_str = malloc(16 * sizeof(char));
+        if (free_ip != NULL) {
+            sprintf(ip_str, "%d.%d.%d.%d", free_ip->octets[0], free_ip->octets[1], free_ip->octets[2], free_ip->octets[3]);
+            ack_packet->yiaddr.s_addr = inet_addr(ip_str);   
+        } else {
+            printf("No free addresses available!\n");
+        }
+    }
+    else {
+        ack_packet->yiaddr.s_addr = inet_addr(inet_ntoa(*(struct in_addr *)&requested_ip)); 
+    }
+
+    // ack_packet->yiaddr.s_addr = inet_addr(inet_ntoa(*(struct in_addr *)&requested_ip)); // Assigned IP address
+    // ack_packet->yiaddr.s_addr = inet_addr("192.168.1.154");
     ack_packet->siaddr.s_addr = inet_addr(DHCP_SERVER_IP); // Server address 
     ack_packet->ciaddr.s_addr = 0;
     ack_packet->giaddr.s_addr = 0;
@@ -124,7 +227,6 @@ int create_ack_packet(dhcp_packet *ack_packet, dhcp_packet *request_packet) {
     ack_packet->cookie = htonl(0x63825363);
 
     print_mac_address(ack_packet->chaddr);
-    int offset = 0;
     ack_packet->options[0] = 53; // DHCP message type option
     ack_packet->options[1] = 1; // Length of the option data
     ack_packet->options[2] = 5; // DHCP ACK message type
@@ -143,28 +245,23 @@ int create_ack_packet(dhcp_packet *ack_packet, dhcp_packet *request_packet) {
     ack_packet->options[13] = 1; //
     ack_packet->options[14] = 1; //
 
-    ack_packet->options[15] = 51; // Ip address lease time option
+    ack_packet->options[15] = 51; // Lease Time option
     ack_packet->options[16] = 4; // Option data length
-    ack_packet->options[17] = 0;
-    ack_packet->options[18] = 14;
-    ack_packet->options[19] = 1;
-    ack_packet->options[20] = 0;
-
-    ack_packet->options[21] = 54; // Dhcp server identity option
+    *(uint32_t *)(&ack_packet->options[17]) = htonl(LEASE_TIME);
+    ack_packet->options[21] = 58; // Renewal Time option
     ack_packet->options[22] = 4; // Option data length
-    ack_packet->options[23] = 192;
-    ack_packet->options[24] = 168;
-    ack_packet->options[25] = 1;
-    ack_packet->options[26] = 1;
-    ack_packet->options[27] = 255; // End of options
+    *(uint32_t *)(&ack_packet->options[23]) = htonl(RENEWAL_TIME); // Convert renewal time to network byte order and store in packet
+    ack_packet->options[27] = 255; // End option
     return sizeof(dhcp_packet);
 }
 
 
+
 int main() {
+    ip_address pool[IP_POOL_SIZE];
+    initialize_ip_pool(pool);
     int sockfd;
     struct sockaddr_in server_addr, client_addr;
-    char buffer[1024];
     dhcp_packet discovery_packet, offer_packet, ack_packet;
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     int enable = 1;
@@ -178,10 +275,9 @@ int main() {
     server_addr.sin_port = htons(SERVER_PORT);
     bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     
-
-
     while(1) {
         printf("Waiting for client request...\n");
+        
         int len = sizeof(client_addr);
         int n = recvfrom(sockfd, &discovery_packet, sizeof(dhcp_packet), 0, (struct sockaddr *)&client_addr, &len);
         printf("Received %d bytes from client\n", n);
@@ -193,16 +289,20 @@ int main() {
 
         if (discovery_packet.options[2] == DHCP_DISCOVERY) {
             printf("DHCP Discovery message received from client\n");
-            int offer_packet_size = create_offer_packet(&offer_packet, &discovery_packet);
+            clean_ip_pool(pool);
+            int offer_packet_size = create_offer_packet(&offer_packet, &discovery_packet, pool);
+            // print_ip_pool(pool);
             sendto(sockfd, &offer_packet, offer_packet_size, 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
             printf("Sending DHCP offer packet to client\n");
         }
 
         if (discovery_packet.options[2] == DHCP_REQUEST) {
             printf("DHCP Request message received from client\n");
-            int ack_packet_size = create_ack_packet(&ack_packet, &discovery_packet);
+            uint32_t requested_ip = extract_requested_ip(&discovery_packet);
+            
+            int ack_packet_size = create_ack_packet(&ack_packet, &discovery_packet, requested_ip, pool);
             sendto(sockfd, &ack_packet, ack_packet_size, 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
-            printf("Sending DHCP offer packet to client\n");
+            printf("Sending DHCP ack packet to client\n");
         }
     }
 
